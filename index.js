89867e31e602743c8ff0138cfd416f9b69449234
VortexJS <106025492+VortexxJS@users.noreply.github.com>
2024-02-29 15:32:00 +0100

    Added new endpoints

--- index.js
+++ index.js
@@ -61,6 +61,48 @@ class AntiP2W {
             throw error;
         }
     }
+// Search with RealmID
+    async searchRealmID(realm_id) {
+        try {
+            const response = await axios.get(`https://antip2w.com/api/database/realms/search/realmid?realm_id=${realm_id}`, {
+                headers: {
+                    'Authorization': `${this.apiKey}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching Realm:', error);
+            throw error;
+        }
+    }
+// Search with Discord Name
+    async searchDiscordName(discord_name) {
+        try {
+            const response = await axios.get(`https://antip2w.com/api/database/realms/search/name?discord_name=${discord_name}`, {
+                headers: {
+                    'Authorization': `${this.apiKey}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching Realm:', error);
+            throw error;
+        }
+    }
+// Search with Realm Code
+    async searchRealmCode(realm_code) {
+        try {
+            const response = await axios.get(`https://antip2w.com/api/database/realms/search/realmcode?realm_code=${realm_code}`, {
+                headers: {
+                    'Authorization': `${this.apiKey}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching Realm:', error);
+            throw error;
+        }
+    }
 }
 
 module.exports = AntiP2W;
